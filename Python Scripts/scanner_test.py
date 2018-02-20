66a170af1da1e63912b93e5623e14acabb2aad7d
dquarshie89 <dquarshie89@gmail.com>
2018-02-20 17:25:18 -0500

    Update scanner_test.py

--- Python Scripts/scanner_test.py
+++ Python Scripts/scanner_test.py
@@ -1,43 +1,86 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb  9 15:10:48 2018
-
-@author: dquarshie
-"""
-
 import urllib.request as req
+import pandas as pd
+import numpy as np
+import datetime as dt
 from bs4 import BeautifulSoup
 
-equities = ('AAPL','AMZN','Main Menu')
-menu = ['Buy','Sell','Show P/L','Show Blotter','Quit']
-blotter = [[]]
 
-def display_menu(menu):
-    for m in menu:
-        print(str(menu.index(m) +1) + " - " + m)
-    #print('Choose an option:')
+pl = pd.DataFrame(columns=['Ticker','Current Price','Position','VWAP','URL','RPL'])
 
-def buy(equities):
-    for e in equities:
-         print(str(equities.index(e) +1) + " - " + e)
-    #print('Pick a stock:')
+#pl = pl.set_index('Ticker')
+'''
+pl = []
+pl.columns = ['Ticker', 'Market Price', 'Position', 'WAP', 'URL', 'RPL']
+'''
+symbol ='A'
 
-done = True
-while done:
-    display_menu(menu)
-    selected = input("Enter your choice [1-5]: ")
-    if selected == "1" or selected == "Buy":
-        print('Buy Menu')
-        buy(equities)
-        stock = input("Pick a stock: ")
-        
-    elif selected == "5" or selected == "Quit":
-        done = False
-        print('Thanks')
-        
-       
-    
-        
-        
+def wavg(group, avg_name, weight_name):
+    """ http://stackoverflow.com/questions/10951341/pandas-dataframe-aggregate-function-using-multiple-columns
+    In rare instance, we may not have weights, so just return the mean
+    """
+    d = group[avg_name]
+    w = group[weight_name]
+    try:
+        return (d * w).sum() / w.sum()
+    except ZeroDivisionError:
+        return d.mean()
+
+quote_page = 'https://finance.yahoo.com/quote/'+ symbol
+page = req.urlopen(quote_page)
+soup = BeautifulSoup(page, 'html.parser')
+name_box = soup.find('h1', attrs={'class':'D(ib)'})
+name = name_box.text.strip() 
+price_box = soup.find('td', attrs={'data-test': 'BID-value'})
+price = price_box.text
+price = price.replace(',', '')
+price = price.split('x', 1)[0]
 
 
+price_buy = pd.DataFrame([[symbol,price]])
+#price_buy = price_buy.reset_index(level='Ticker')
+price_buy = pd.DataFrame(price_buy).reset_index(drop=True) 
+price_buy.columns = ['Ticker', 'Current Price']
+
+'''
+position = blotter.groupby(['Ticker'])[['Shares']].sum() # use as dataframe and join to vwap 
+position = pd.DataFrame(position).reset_index()
+position.columns = ['Ticker', 'Position']
+
+wap = blotter.groupby('Ticker').apply(wavg, 'Price per Share', 'Shares')
+#wap = blotter.groupby(['Ticker']).apply(lambda x: np.average(x[['Price per Share']], weights=x[['Shares']]))
+wap = pd.DataFrame(wap).reset_index()
+wap.columns = ['Ticker', 'VWAP']
+
+price_position = pd.merge(price_buy, position, on='Ticker')
+
+pw = pd.merge(price_position, wap, on='Ticker')
+
+url = float(pw['Current Price'])*float(pw['Position'])
+
+url_profit = pd.DataFrame([[symbol,url,'0']])
+url_profit.columns = ['Ticker', 'URL','RPL']
+
+plb = pd.merge(pw, url_profit, on='Ticker')
+#plb = pd.DataFrame(plb).reset_index()
+plb.columns=['Ticker','Current Price','Position','VWAP','URL','RPL']
+'''
+#plb = plb.set_index('Ticker')
+#pl = pd.concat([pl, plb], axis=0)
+print(price_buy)
+#print(pl)
+#print(plb)
+#pl.loc[1] = ([plb])
+
+#pl = ([plb])
+# pd.DataFrame([[k[0], k[1], v.split()[1]] for k,v in d.items()], columns=['id','Name','Value'])
+'''
+ticker = pl_buy['Ticker']
+mp = pl_buy['Market Price']
+pos = pl_buy['Position']
+vwap = pl_buy['WAP']
+unreal = pl_buy['URL']
+real = pl_buy['RPL']
+'''
+#print(pl_buy)
+
+#pl = ('1',ticker,mp,pos,vwap,unreal,'0')
