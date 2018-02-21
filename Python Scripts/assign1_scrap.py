cf4c2cc4d929be56f7ab55f9ab5f7d006f5dd8b0
dquarshie89 <dquarshie89@gmail.com>
2018-02-21 17:07:22 -0500

    Update assign1_scrap.py

--- Python Scripts/assign1_scrap.py
+++ Python Scripts/assign1_scrap.py
@@ -12,6 +12,7 @@ import numpy as np
 import datetime as dt
 from bs4 import BeautifulSoup
 
+tradenum=0
 cash = 10000000
 
 now = dt.datetime.now()
@@ -29,7 +30,7 @@ blotter = pd.DataFrame(columns=[
         'Money In/Out']
         )
 
-pl = pd.DataFrame(columns=[
+plb = pd.DataFrame(columns=[
         'Ticker',
         'Current Market Price',
         'Position',
@@ -39,6 +40,26 @@ pl = pd.DataFrame(columns=[
         'As Of'
         ])
 
+plt = pd.DataFrame(columns=[
+        'Ticker',
+        'Current Market Price',
+        'Position',
+        'VWAP',
+        'UPL',
+        'RPL',
+        'As Of'
+        ])
+
+
+pls = pd.DataFrame(columns=[
+        'Ticker',
+        'Current Market Price',
+        'Position',
+        'VWAP',
+        'UPL',
+        'RPL',
+        'As Of'
+        ])
 
 def display_menu(menu):
     print('\nMain Menu\n')
@@ -84,7 +105,7 @@ def wavg(group, avg_name, weight_name):
         return d.mean()
 
 def pl_buy(symbol):
-    global pl
+    global plb
     quote_page = 'https://finance.yahoo.com/quote/'+ symbol
     page = req.urlopen(quote_page)
     soup = BeautifulSoup(page, 'html.parser')
@@ -94,11 +115,11 @@ def pl_buy(symbol):
     price = price_box.text
     price = price.replace(',', '')
     price = price.split('x', 1)[0]
-    
+
     price_buy = pd.DataFrame([[symbol,price]])
     price_buy.columns = ['Ticker', 'Market Price']
     
-    position = blotter.groupby(['Ticker'])[['Shares']].sum() # use as dataframe and join to vwap 
+    position = blotter[blotter['Action'] == 'Buy'].groupby(['Ticker'])[['Shares']].sum() # use as dataframe and join to vwap 
     position = pd.DataFrame(position).reset_index()
     position.columns = ['Ticker', 'Position']
     
@@ -119,18 +140,79 @@ def pl_buy(symbol):
     pl_buy = pd.merge(pw, url_profit, on='Ticker')
      
     #pl = pl_buy.append(pl_buy)
-    pl = np.vstack((pl,pl_buy)) 
-    pl = df(pl)
-    pl.columns=['Ticker','Current Price','Position','VWAP','URL','RPL','As Of']
+    plb = np.vstack((plb,pl_buy)) 
+    plb = df(plb)
+    plb.columns=['Ticker','Current Price','Position','VWAP','URL','RPL','As Of']
     
-    pl = pl.sort_values(by='As Of')
-    pl = pl.drop_duplicates('Ticker', keep='last').values
-    pl = df(pl)
-    pl.columns=['Ticker','Current Price','Position','VWAP','URL','RPL','As Of']
+    plb = plb.sort_values(by='As Of')
+    plb = plb.drop_duplicates('Ticker', keep='last').values
+    plb = df(plb)
+    plb.columns=['Ticker','Current Price','Position','VWAP','URL','RPL','As Of']
  
-tradenum=0
-plnum = 0
+def pl_sell(symbol):
+    global pls
+    quote_page = 'https://finance.yahoo.com/quote/'+ symbol
+    page = req.urlopen(quote_page)
+    soup = BeautifulSoup(page, 'html.parser')
+    name_box = soup.find('h1', attrs={'class':'D(ib)'})
+    name = name_box.text.strip() 
+    price_box = soup.find('td', attrs={'data-test': 'BID-value'})
+    price = price_box.text
+    price = price.replace(',', '')
+    price = price.split('x', 1)[0]
 
+    price_buy = pd.DataFrame([[symbol,price]])
+    price_buy.columns = ['Ticker', 'Market Price']
+    
+    position = blotter[blotter['Action'] == 'Sell'].groupby(['Ticker'])[['Shares']].sum() # use as dataframe and join to vwap 
+    position = pd.DataFrame(position).reset_index()
+    position.columns = ['Ticker', 'Position']
+    
+    wap = blotter.groupby('Ticker').apply(wavg, 'Price per Share', 'Shares')
+    #wap = blotter.groupby(['Ticker']).apply(lambda x: np.average(x[['Price per Share']], weights=x[['Shares']]))
+    wap = pd.DataFrame(wap).reset_index()
+    wap.columns = ['Ticker', 'WAP']
+    
+    price_position = pd.merge(price_buy, position, on='Ticker')
+    
+    pw = pd.merge(price_position, wap, on='Ticker')
+    
+    rpl = float(pw['Market Price'])*float(pw['Position'])
+    
+    url_profit = pd.DataFrame([[symbol,'0',rpl,pd.to_datetime('now')]])
+    url_profit.columns = ['Ticker', 'URL','RPL','As Of']
+    
+    pl_sell = pd.merge(pw, url_profit, on='Ticker')
+     
+    #pl = pl_buy.append(pl_buy)
+    pls = np.vstack((pls,pl_sell)) 
+    pls = df(pls)
+    pls.columns=['Ticker','Current Price','Position','VWAP','URL','RPL','As Of']
+    
+    pls = pls.sort_values(by='As Of')
+    pls = pls.drop_duplicates('Ticker', keep='last').values
+    pls = df(pls)
+    pls.columns=['Ticker','Current Price','Position','VWAP','URL','RPL','As Of']
+
+def pl_tot(symbol):
+    global plt
+    quote_page = 'https://finance.yahoo.com/quote/'+ symbol
+    page = req.urlopen(quote_page)
+    soup = BeautifulSoup(page, 'html.parser')
+    name_box = soup.find('h1', attrs={'class':'D(ib)'})
+    name = name_box.text.strip() 
+    price_box = soup.find('td', attrs={'data-test': 'BID-value'})
+    price = price_box.text
+    price = price.replace(',', '')
+    price = price.split('x', 1)[0]
+    
+    plt = pd.DataFrame([[symbol, 
+           price, 
+           plb['Position']-pls['Position'], 
+           plb['VWAP'], 
+           plb['URL']-pls['RPL'],
+           pls['RPL']
+           ]])
 
 
 done = True
@@ -153,21 +235,21 @@ while done:
                 buy(symbol)
                 print('\nBlotter\n')
                 print(blotter)
-                plnum += 1 
                 pl_buy(symbol)
             if buy_confirm == 'N':
                 display_menu(menu)
-        if trade == 'Sell':
+        elif trade == 'Sell':
             print('\nStocks: ')
             Trade(equities)
             symbol = input('\nPick your stock symbol: ')
             shares = int(input('\nEnter Number of shares: '))
-            sell_confirm = input('\n Sell %s shares of %s? (Y/N): ' % (shares, symbol))
+            sell_confirm = input('\nSell %s shares of %s? (Y/N): ' % (shares, symbol))
             if sell_confirm == 'Y':
                 tradenum += 1
                 sell(symbol)
                 print('\nBlotter\n')
                 print(blotter)
+                pl_sell(symbol)
             if sell_confirm == 'N':
                 display_menu(menu)
     
@@ -177,9 +259,10 @@ while done:
    
     elif selected == 3:
         print('\nP/L\n')
-        
-        print(pl)
-        
+        print(plb)
+        print(pls)
+        pl_tot(symbol)
+        print(plt)
         
         
         
@@ -191,4 +274,3 @@ while done:
         
     elif selected >4 or selected<1:
         print('\nPlease enter a valid choice')
-        #display_menu(menu)
