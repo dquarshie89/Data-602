e5a3e2e6fa633394c2d530b03f65a5864c73fe2a
dquarshie89 <dquarshie89@gmail.com>
2018-04-03 00:03:27 -0400

    Update assign1_scrap.py

--- Python Scripts/assign1_scrap.py
+++ Python Scripts/assign1_scrap.py
@@ -13,18 +13,30 @@ import numpy as np
 import datetime as dt
 from bs4 import BeautifulSoup
 
-app = Flask(__name__)
 
+
+#Initialize tradenum to count the number of trades made (Buy or Sell) during the session 
+#Will be used to place trades in the blotter
 tradenum=0
+
+#Initialize pl to have a line for each ticker's profit and loss during the session 
+#Will be used in the P/L tracking
 plnum =0 
+
+#Initialize the user with $10M
 cash = 10000000
 
+#Get the current date and time for use in the blotter. 
+#Will show the date and time for each trade in the blotter
 now = dt.datetime.now()
 
+#List of tickers to show in the trade menu
 equities = ('AAPL','AMZN','INTC','MSFT','SNAP')
 
+#Menu items
 menu = ['Trade','Show Blotter','Show P/L','Quit']
 
+#Make a data frame for the blotter that has nothing but columns  
 blotter = pd.DataFrame(columns=[
         'Action',
         'Ticker',
@@ -33,7 +45,8 @@ blotter = pd.DataFrame(columns=[
         'Trade Timestap',
         'Money In/Out']
         )
-
+'''
+#Make a data frame for the buy section of the session. This will be used to see the unrealized profit/loss
 plb = pd.DataFrame(columns=[
         'Ticker',
         'Current Market Price',
@@ -44,6 +57,7 @@ plb = pd.DataFrame(columns=[
         'As Of'
         ])
 
+#Make a data frame for the sell section of the session. This will be used to see the realized profit/loss
 pls = pd.DataFrame(columns=[
         'Ticker',
         'Current Market Price',
@@ -54,7 +68,7 @@ pls = pd.DataFrame(columns=[
         'As Of'
         ])
 
-
+#Used a merge between the buy and the sell to see both unrealized and realized profit/loss
 plt = pd.DataFrame(columns=[
         'Ticker',
         'Current Market Price',
@@ -63,31 +77,18 @@ plt = pd.DataFrame(columns=[
         'UPL',
         'RPL',
         ])
-
+'''
+#Function to number the menu items 1-4 and display them
 def display_menu(menu):
     print('\nMain Menu\n')
     for m in menu:
         print(str(menu.index(m) +1) + " - " + m)
-    
+
+#Function to show the tickers  
 def Trade(equities):
     print('\n'.join(equities))
 
-def price_check(symbol):
-    quote_page = 'https://finance.yahoo.com/quote/'+ symbol
-    page = req.urlopen(quote_page)
-    soup = BeautifulSoup(page, 'html.parser')
-    name_box = soup.find('h1', attrs={'class':'D(ib)'})
-    name = name_box.text.strip() 
-    buy_box = soup.find('td', attrs={'data-test': 'ASK-value'})
-    buy = buy_box.text
-    buy = buy.replace(',', '')
-    buy = buy.split('x', 1)[0]
-    sell_box = soup.find('td', attrs={'data-test': 'BID-value'})
-    sell = sell_box.text
-    sell = sell.replace(',', '')
-    sell = sell.split('x', 1)[0]
-    return buy, sell
-
+#Function to check the ASK and BID prices of the ticker and add to the blotter for each confirmed Buy or Sell
 def get_quote(symbol):
     quote_page = 'https://finance.yahoo.com/quote/'+ symbol
     page = req.urlopen(quote_page)
@@ -107,6 +108,9 @@ def get_quote(symbol):
     if trade == 'Sell':
         blotter.loc[tradenum] = (['Sell', symbol, int(shares), float(price), pd.to_datetime('now'), round(float(price)*float(shares),2)])
 
+#Function to calculate the weighted average price for the ticker
+#Using the price per share and the number of shares the weighted average is calculated 
+#A regular average is displayed if there are no shares
 def wavg(group, avg_name, weight_name):
     d = group[avg_name]
     w = group[weight_name]
@@ -115,6 +119,8 @@ def wavg(group, avg_name, weight_name):
     except ZeroDivisionError:
         return d.mean()
 
+#Function ran to add the unrealized profit/loss for each buy
+#Use bid value (current price) to calculate unrealized profit/loss
 def pl_buy(symbol):
     global plb
     quote_page = 'https://finance.yahoo.com/quote/'+ symbol
@@ -126,42 +132,55 @@ def pl_buy(symbol):
     price = price_box.text
     price = price.replace(',', '')
     price = price.split('x', 1)[0]
-
+    
+    #Put symbol and price in a data frame to later be use a join to another data frame
+    #The idea is to have SQL like tables and join them using the symbol to get all data on a row
     price_buy = pd.DataFrame([[symbol,price]])
     price_buy.columns = ['Ticker', 'Market Price']
     
+    #Group by the ticker to get the total number of shares
+    #If another buy of the same ticker happens we want to have the total number of shares
     position = blotter[blotter['Action'] == 'Buy'].groupby(['Ticker'])[['Shares']].sum() # use as dataframe and join to vwap 
     position = pd.DataFrame(position).reset_index()
     position.columns = ['Ticker', 'Position']
     
+    #Use the wavg function to get the wegihted average by ticker
+    #Put ticker and weighted average price in a data frame
     wap = blotter.groupby('Ticker').apply(wavg, 'Price per Share', 'Shares')
+    #Weighted avg could also be calculated with numpy using:
     #wap = blotter.groupby(['Ticker']).apply(lambda x: np.average(x[['Price per Share']], weights=x[['Shares']]))
     wap = pd.DataFrame(wap).reset_index()
     wap.columns = ['Ticker', 'WAP']
     
+    #Merge the price data frame to the position data frame and place in a new data frame
     price_position = pd.merge(price_buy, position, on='Ticker')
     
+    #Merge the price and position data frame to the weighted average data frame and place in a new data frame
     pw = pd.merge(price_position, wap, on='Ticker')
     
+    #Calculate the uneralized profit/loss using the position and market price
     url = float(pw['Market Price'])*float(pw['Position'])
     
+    #Put ticker, unrealized profit/loss, and realized profit/loss as 0 in a data frame
     url_profit = pd.DataFrame([[symbol,url,'0',pd.to_datetime('now')]])
     url_profit.columns = ['Ticker', 'URL','RPL','As Of']
     
+    #Merge the price data frame with the unrealized profit/loss data frame
     pl_buy = pd.merge(pw, url_profit, on='Ticker')
-     
-    #pl = pl_buy.append(pl_buy)
-    plb = np.vstack((plb,pl_buy)) 
-    plb = df(plb)
+    
+    #Make a profit loss data frame 
+    #plb = np.vstack((plb,pl_buy)) 
+    plb = df(pl_buy)
     plb.columns=['Ticker','Current Price','Position','VWAP','URL','RPL','As Of']
     
+    #Sort profit and loss by most to least and take the most recent line per ticker
     plb = plb.sort_values(by='As Of')
     plb = plb.drop_duplicates('Ticker', keep='last').values
     plb = df(plb)
     plb.columns=['Ticker','Current Price','Position','VWAP','URL','RPL','As Of']
  
+#Run the saem function as pl_buy but makes a data frame with realized profit and loss and unrealized = 0
 def pl_sell(symbol):
-    global pls
     quote_page = 'https://finance.yahoo.com/quote/'+ symbol
     page = req.urlopen(quote_page)
     soup = BeautifulSoup(page, 'html.parser')
@@ -180,6 +199,7 @@ def pl_sell(symbol):
     position.columns = ['Ticker', 'Position']
     
     wap = blotter.groupby('Ticker').apply(wavg, 'Price per Share', 'Shares')
+    #Weighted avg could also be calculated with numpy using:
     #wap = blotter.groupby(['Ticker']).apply(lambda x: np.average(x[['Price per Share']], weights=x[['Shares']]))
     wap = pd.DataFrame(wap).reset_index()
     wap.columns = ['Ticker', 'WAP']
@@ -195,9 +215,8 @@ def pl_sell(symbol):
     
     pl_sell = pd.merge(pw, url_profit, on='Ticker')
      
-    #pl = pl_buy.append(pl_buy)
-    pls = np.vstack((pls,pl_sell)) 
-    pls = df(pls)
+    #pls = np.vstack((pls,pl_sell)) 
+    pls = df(pl_sell)
     pls.columns=['Ticker','Current Price','Position','VWAP','URL','RPL','As Of']
     
     pls = pls.sort_values(by='As Of')
@@ -205,8 +224,8 @@ def pl_sell(symbol):
     pls = df(pls)
     pls.columns=['Ticker','Current Price','Position','VWAP','URL','RPL','As Of']
 
+#Merge the pl_buy and pl_sell data frames to get the total unrealized and realized profit/loss
 def pl_tot(symbol):
-    global plt
     quote_page = 'https://finance.yahoo.com/quote/'+ symbol
     page = req.urlopen(quote_page)
     soup = BeautifulSoup(page, 'html.parser')
@@ -217,6 +236,7 @@ def pl_tot(symbol):
     price = price.replace(',', '')
     price = price.split('x', 1)[0]
     
+    #Check if the ticker has been sold and calculate the URL and RPL
     if (symbol in plb[['Ticker']].values) == True and (symbol in pls[['Ticker']].values) ==True:
         plt = df([[symbol, 
                    price, 
@@ -226,13 +246,17 @@ def pl_tot(symbol):
                    float(pls['RPL'])
                    ]])
         plt.columns=['Ticker','Current Price','Position','VWAP','URL','RPL']
-        
-    elif (symbol in pls[['Ticker']].values) ==False and (symbol in plb[['Ticker']].values) ==True:
+    
+    #If the ticker has not been sold then use the plb as the totl profit/loss 
+    if (symbol in pls[['Ticker']].values) ==False and (symbol in plb[['Ticker']].values) ==True:
         plt = plb[['Ticker','Current Price', 'Position', 'VWAP', 'URL', 'RPL']]
+    
  
- 
+
+#Start program
 done = True
 
+#Program will exit when done = False 
 while done:
     display_menu(menu)
     selected = int(input('\nEnter your choice [1-4]: '))
@@ -254,6 +278,7 @@ while done:
             price = price.split('x', 1)[0]
             total_price = float(price)*float(shares)
             buy_confirm = input('\nBuy %s shares of %s at $%s for $%s? (Y/N): ' % (shares, symbol, price, round(total_price,2)))
+            #Check to see if the user has enough cash to buy selected number of shares at ask price
             if buy_confirm == 'Y' and total_price > cash:
                 print('\nNot enough money to buy %s \n' %(symbol))
                 print('\nTotal Cost: ')
@@ -262,12 +287,15 @@ while done:
                 print(cash)
             if buy_confirm == 'Y' and total_price <= cash:
                 tradenum += 1
+                #Add the buy to the blotter
                 get_quote(symbol)
+                #Remove the total buy from the user's cash
                 cash = cash - blotter[blotter['Action'] == 'Buy']['Money In/Out'].sum()
                 print('\nBlotter\n')
                 print(blotter)
                 print('\nRemaining Cash:\n')
                 print(cash)
+                #Add the buy to the profit/loss
                 pl_buy(symbol)
                 pl_tot(symbol)
             if buy_confirm == 'N':
@@ -294,33 +322,37 @@ while done:
                 print(blotter)
                 print('\nRemaining Cash:\n')
                 print(cash)
+                #Add the sell to the profit/loss
                 pl_sell(symbol)
             if sell_confirm == 'Y' and (symbol in plb[['Ticker']].values)==False:
+                #Checks to see if there is any of the ticker to sell
                 print('\nThere is no %s to sell\n' % (symbol))
             if sell_confirm == 'N':
                 print('\nDid not sell %s' %(symbol))
     
     elif selected == 2:
+        #Show blotter
         print('\nBlotter\n')
         print(blotter)             
    
     elif selected == 3:
         if plt.empty == False:
+            #Run the P/L
             plnum=+1
             print('\nP/L\n')
             pl_tot(symbol)
             print(plt)
         else:
+            #If there's nothing in the P/L 
             print('\nNo P and L yet\n')
 
         
     elif selected == 4:
+        #Exit program
         print('\nThanks')
         done = False
         
     elif selected >4 or selected<1:
+        #Only allow users to eneter between 1 and 4
         print('\nPlease enter a valid choice')
-'''
-if __name__ == "__main__":
-    app.run(host='0.0.0.0') # host='0.0.0.0' needed for docker
-'''
+
