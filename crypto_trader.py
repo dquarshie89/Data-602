94380daf1e220291addd95e2d0a97e663ede5bef
dquarshie89 <dquarshie89@gmail.com>
2018-04-08 22:02:39 -0400

    Update crypto_trader.py

--- crypto_trader.py
+++ crypto_trader.py
@@ -15,19 +15,31 @@ import json
 from bson import json_util
 from pymongo import MongoClient
 
+#Initialize tradenum to count the number of trades made (Buy or Sell) during the session 
+#Will be used to place trades in the blotter
 tradenum=0
+
+#Initialize pl to have a line for each ticker's profit and loss during the session 
+#Will be used in the P/L tracking
 plnum =0 
+
+#Initialize the user with $10M
 cash = 10000000
 
+#Get the current date and time for use in the blotter. 
+#Will show the date and time for each trade in the blotter
 now = dt.datetime.now()
 
+#Set up the Collection in MongoDB
 client = MongoClient()
 db = client.blotter_database
 collection = db.blotter_collection
 trades = db.trades
 
+#Menu items
 menu = ['Crypto Info', 'Trade','Show Blotter','Show P/L','Quit']
 
+#Make a data frame for the blotter that has nothing but columns 
 blotter = pd.DataFrame(columns=[
         'Action',
         'Currency',
@@ -37,10 +49,12 @@ blotter = pd.DataFrame(columns=[
         'Money In/Out',
         'Cash']
         )
+#Make a data frame that will hold the currencies that are traded
 givecurr = pd.DataFrame(columns=[
         'Currency'
         ])
 
+#Make a data frame for the buy section of the session. This will be used to see the unrealized profit/loss
 plb = pd.DataFrame(columns=[
         'Ticker',
         'Current Market Price',
@@ -52,6 +66,7 @@ plb = pd.DataFrame(columns=[
         'As Of'
         ])
 
+#Make a data frame for the sell section of the session. This will be used to see the realized profit/loss
 pls = pd.DataFrame(columns=[
         'Ticker',
         'Current Market Price',
@@ -63,13 +78,13 @@ pls = pd.DataFrame(columns=[
         'As Of'
         ])
 
-#rec_cur ='USD'
-
+#Function to number the menu items 1-5 and display them
 def display_menu(menu):
     print('\nMain Menu\n')
     for m in menu:
         print(str(menu.index(m) +1) + " - " + m)
 
+#Function that will use the API to get the price, price history, min, max, and standard deviation of the currency
 def get_quote(give_cur,rec_cur):
     price = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms="+give_cur+"&tsyms="+rec_cur)
     price_data = price.json()
@@ -98,6 +113,7 @@ def get_quote(give_cur,rec_cur):
     
     return (price, maxprice, minprice, stdprice, hist_df['time'] ,hist_df['close'])
 
+#Function that will put the trade in blotter and the currency in the dataframe
 def action(trade):
     x = get_quote(give_cur,rec_cur)
     if trade =='Buy':
@@ -106,7 +122,8 @@ def action(trade):
     if trade == 'Sell':
         blotter.loc[tradenum] = (['Sell', give_cur, float(shares), float(x[1]), pd.to_datetime('now'), round(float(x[1])*float(shares),2),cash+round(float(x[1])*float(shares),2)])
         givecurr.loc[tradenum] = ([give_cur])
-        
+
+#Function used to get a graph of the currenct price over 100 days         
 def get_graph(give_cur,rec_cur):  
     quote = get_quote(give_cur,rec_cur)
     plt.plot(quote[4] ,quote[5] )
@@ -116,6 +133,7 @@ def get_graph(give_cur,rec_cur):
     plt.title('Price of '+give_cur+' in '+rec_cur+' over 100 Days')
     plt.show(block=False)
 
+#Function used to get the graph of the 20 day moving average price of the currency
 def mavg(give_cur,rec_cur):
     quote = get_quote(give_cur,rec_cur)
     ret = np.cumsum(quote[5].values.tolist(), dtype=float)
@@ -127,7 +145,8 @@ def mavg(give_cur,rec_cur):
     plt.ylabel('Price')
     plt.title('Moving 20 Day Avg Price of '+give_cur+' in '+rec_cur)
     plt.show(block=False)
-    
+
+#Function used to ge the weighted average price 
 def wavg(group, avg_name, weight_name):
     d = group[avg_name]
     w = group[weight_name]
@@ -142,6 +161,7 @@ done = True
 while done:
     display_menu(menu)
     selected = int(input('\nEnter your choice [1-5]: '))
+    #If the user selects 1 they will get some basic info about the currency they are looking up
     if selected == 1:
         print('\nCrypto Info')
         give_cur = input('\nPick crypto to look up: ') 
@@ -163,7 +183,7 @@ while done:
             get_graph(give_cur,rec_cur)
             x = get_quote(give_cur,rec_cur)
             buy_confirm = input('\nBuy %s of %s for %s at %s for %s? (Y/N): ' % (shares, give_cur, rec_cur, x[1], round(float(x[1])*float(shares),2)))
-            #Check to see if the user has enough cash to buy selected number of shares at ask price
+            #Check to see if the user has enough cash to buy selected currency
             if buy_confirm == 'Y' and float(x[1])*float(shares) > cash:
                 print('\nNot enough money to buy %s \n' %(give_cur))
                 print('\nTotal Cost: ')
@@ -172,10 +192,8 @@ while done:
                 print(round(cash,2))
             if buy_confirm == 'Y' and float(x[1])*float(shares) <= cash:
                 tradenum += 1
-                #Add the buy to the blotter
                 act = action(trade)
-                #Add the buy to the profit/loss
-                #pl_buy(give_cur,rec_cur)
+                
                 #Remove the total buy from the user's cash
                 cash = cash - blotter[blotter['Action'] == 'Buy']['Money In/Out'].sum()
                 print('\nBlotter\n')
@@ -184,6 +202,7 @@ while done:
                 print(round(cash,2))
                 
                 quote = get_quote(give_cur,rec_cur)
+                #Add the buy to the profit/loss
                 price_buy = pd.DataFrame([[give_cur,quote[0]]])
                 price_buy.columns = ['Ticker', 'Market Price']
                 
@@ -228,6 +247,7 @@ while done:
                 plb = df(plb)
                 plb.columns=['Ticker','Current Market Price','Position','VWAP','URL','RPL', 'Total P/L','As Of']
                 
+                #Add trade to the database
                 blot = blotter.to_json()                
                 blot = json_util.loads(blot)
                 trades.insert_one(blot)
@@ -244,7 +264,7 @@ while done:
             if sell_confirm == 'Y' and (give_cur in blotter[['Currency']].values)==True:
                 tradenum += 1
                 act = action(trade)
-                #pl_sell(give_cur,rec_cur)
+                
                 cash = cash + blotter[blotter['Action'] == 'Sell']['Money In/Out'].sum()
                 print('\nBlotter\n')
                 print(blotter)
@@ -277,7 +297,6 @@ while done:
                  
                 pls = np.vstack((pls,pl_sell)) 
                 pls = df(pls)
-                #pls2 = df(pl_sell)
                 pls.columns=['Ticker','Current Market Price','Position','VWAP','URL','RPL','Total P/L','As Of']
                 
                 #Sort profit and loss by most to least and take the most recent line per ticker
@@ -289,9 +308,7 @@ while done:
                 blot = blotter.to_json()
                 blot = json_util.loads(blot)
                 trades.insert_one(blot)
-             #if sell_confirm == 'Y' and (symbol in plb[['Ticker']].values)==False:
-                #Checks to see if there is any of the ticker to sell
-                #print('\nThere is no %s to sell\n' % (symbol))
+
             if sell_confirm == 'N':
                 print('\nDid not sell %s' %(give_cur))
      
@@ -301,22 +318,24 @@ while done:
         print(blotter) 
     
     elif selected == 4:
-            #if pls.empty==True:
-            #if (pls['Ticker'].values in plb['Ticker'].values) ==False:
+            #If Currency has been bought but not sold
             if (givecurr['Currency'].values in pls[['Ticker']].values) ==False and (givecurr['Currency'].values in plb[['Ticker']].values) ==True:
                 pltot = plb[['Ticker','Current Market Price', 'Position', 'VWAP', 'URL', 'RPL','Total P/L']]
+                #Add alloaction by shares
                 prct = blotter.groupby(['Currency']).agg({'Quantity': 'sum'})/blotter.agg({'Quantity': 'sum'})
                 prctshare = pd.DataFrame(prct).reset_index()
                 prctshare.columns = ['Ticker', '% of Total Shares']
                 pltot = pd.merge(pltot, prctshare, on='Ticker')
                 pltot.columns=['Ticker','Current Market Price','Position','VWAP','URL','RPL','Total P/L','% of Total Shares']
                 
+                #Add allocation by P/L
                 prctdol = pltot.groupby(['Ticker']).agg({'Total P/L': 'sum'})/pltot.agg({'Total P/L': 'sum'})
                 prctpl = pd.DataFrame(prctdol).reset_index()
                 prctpl.columns = ['Ticker', '% of Total PL']
                 pltot = pd.merge(pltot, prctpl, on='Ticker')
                 pltot.columns=['Ticker','Current Market Price','Position','VWAP','URL','RPL','Total P/L','% of Total Shares','% of Total PL']
-            #if (pls['Ticker'].values in plb['Ticker'].values) ==True:   
+            
+            #If Currency has been bought and sold
             if (givecurr['Currency'].values in plb[['Ticker']].values) == True and (givecurr['Currency'].values in pls[['Ticker']].values) ==True:
                 pltot = df([[plb['Ticker'], 
                    quote[0], 
@@ -326,6 +345,7 @@ while done:
                    float(pls['RPL']),
                    float(plb['URL'])+float(pls['RPL'])
                    ]])
+                #Add alloaction by shares
                 pltot = plb[['Ticker','Current Market Price', 'Position', 'VWAP', 'URL', 'RPL','Total P/L']]
                 prct = blotter.groupby(['Currency']).agg({'Quantity': 'sum'})/blotter.agg({'Quantity': 'sum'})
                 prctshare = pd.DataFrame(prct).reset_index()
@@ -333,6 +353,7 @@ while done:
                 pltot = pd.merge(pltot, prctshare, on='Ticker')
                 pltot.columns=['Ticker','Current Market Price','Position','VWAP','URL','RPL','Total P/L','% of Total Shares']
                 
+                #Add allocation by P/L
                 prctdol = pltot.groupby(['Ticker']).agg({'Total P/L': 'sum'})/pltot.agg({'Total P/L': 'sum'})
                 prctpl = pd.DataFrame(prctdol).reset_index()
                 prctpl.columns = ['Ticker', '% of Total PL']
@@ -341,9 +362,6 @@ while done:
             
             print(pltot)
            
-            #else:
-            #If there's nothing in the P/L 
-             #   print('\nNo P and L yet\n')
     
     elif selected == 5:
         print('\nThanks')
