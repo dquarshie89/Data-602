be3ad046bc18c61cec0aba2d8712b07d014f5c37
dquarshie89 <dquarshie89@gmail.com>
2018-04-18 15:37:13 -0400

    Update crypto tester.py

--- Python Scripts/crypto tester.py
+++ Python Scripts/crypto tester.py
@@ -13,29 +13,151 @@ import numpy as np
 import datetime as dt
 import json
 
-tradenum=0
-plnum =0 
-cash = 10000000
+def main():
+    tradenum=0
+    cash = 10000000
+    give_cur='BTC'
+    rec_cur='USD'
+    
+    now = dt.datetime.now()
+    
+    menu = ['Crypto Info', 'Trade','Show Blotter','Show P/L','Quit']
+    
+    blotter = pd.DataFrame(columns=[
+            'Action',
+            'Bought Currency',
+            'Sold Currency',
+            'Quantity',
+            'Price per Share',
+            'Trade Timestap',
+            'Money In/Out',
+            'Cash']
+            ) 
+    
+    def action(trade):
+        x = get_quote(give_cur,rec_cur)
+        if trade =='Buy':
+            blotter.loc[tradenum] = (['Buy', give_cur, rec_cur, float(shares), float(x[1]), pd.to_datetime('now'), round(float(x[1])*float(shares),2),cash-round(float(x[1])*float(shares),2)])
+        if trade == 'Sell':
+            blotter.loc[tradenum] = (['Sell', give_cur, rec_cur, float(shares), float(x[1]), pd.to_datetime('now'), round(float(x[1])*float(shares),2),cash+round(float(x[1])*float(shares),2)])
 
-now = dt.datetime.now()
+    
+    df_pl = initialize_pl(give_cur,rec_cur)
+    
+    def update_pl(pl, shares):
+        if trade =='Buy': # buy
+            x = get_quote(give_cur,rec_cur)
+            current_qty = pl.at[give_cur,'Position']
+            #current_vwap = pl.at[give_cur,'VWAP']
+            new_vwap = blotter.groupby('Bought Currency').apply(wavg, 'Price per Share', 'Quantity')
+            pl.at[give_cur,'Position'] = current_qty + shares
+            pl.at[give_cur,'VWAP'] = round(new_vwap[0],2)
+            pl.at[give_cur,'UPL'] = float(x[1])*float(shares)
+            pl.at[give_cur,'Current Market Price'] =x[1]
+            pl.at[give_cur,'Total P/L']=pl.at[give_cur,'UPL']+pl.at[give_cur,'RPL']
+            pos_per = pl.groupby(['Bought Currency']).agg({'Position': 'sum'})/pl.agg({'Position': 'sum'})
+            pos_per = pd.DataFrame(pos_per).reset_index()
+            pos_per.columns = ['Ticker', '% of Total PL']
+            pl.at[give_cur,'% of Total Shares']=pos_per[['% of Total PL']]
+        elif trade =='Sell': #sell
+            x = get_quote(give_cur,rec_cur)
+            current_qty = pl.at[give_cur,'Position']
+            current_upl = pl.at[give_cur,'UPL']
+            new_vwap = blotter.groupby('Bought Currency').apply(wavg, 'Price per Share', 'Quantity')
+            pl.at[give_cur,'Position'] = current_qty - shares
+            pl.at[give_cur,'VWAP'] = round(new_vwap[0],2)
+            pl.at[give_cur,'RPL'] = float(x[1])*float(shares)
+            pl.at[give_cur,'Current Market Price'] =x[1]
+            pl.at[give_cur,'UPL'] = current_upl - (float(x[1])*float(shares))
+            pl.at[give_cur,'Total P/L']=pl.at[give_cur,'UPL']+pl.at[give_cur,'RPL']
+            pos_per = pl.groupby(['Bought Currency']).agg({'Position': 'sum'})/pl.agg({'Position': 'sum'})
+            pos_per = pd.DataFrame(pos_per).reset_index()
+            pos_per.columns = ['Ticker', '% of Total PL']
+            pl.at[give_cur,'% of Total Shares']=pos_per[['% of Total PL']]
+        return pl
+    
+    done = True   
+    while done:
+        display_menu(menu)
+        selected = int(input('\nEnter your choice [1-5]: '))
+        if selected == 1:
+            print('\nCrypto Info')
+            give_cur = input('\nPick crypto to look up: ') 
+            rec_cur = input('\nPick currency to receive: ') 
+            get_graph(give_cur,rec_cur)
+            mavg(give_cur,rec_cur)
+            q = get_quote(give_cur,rec_cur)
+            print('\nCurrent Price: %s' %(q[0]))
+            print('\nMax Price in the Last 24hrs: %s' %(q[1]))
+            print('\nMin Price in the Last 24hrs: %s' %(q[2]))
+            print('\nStd Dev of Price in the Last 24hrs: %s' %(q[3]))  
+        
+        elif selected == 2:
+            print('\nTrade Menu')
+            trade = input('Buy or Sell?: ')
+            if trade == 'Buy':
+                give_cur = input('\nPick your currency to buy: ')
+                rec_cur = input('\nPick your currency to sell: ')
+                shares = float(input('\nEnter Quantity: '))
+                get_graph(give_cur,rec_cur)
+                x = get_quote(give_cur,rec_cur)
+                buy_confirm = input('\nBuy %s of %s for %s at %s for %s? (Y/N): ' % (shares, give_cur, rec_cur, x[0], round(float(x[0])*float(shares),2)))
+                #Check to see if the user has enough cash to buy selected number of shares at ask price
+                if buy_confirm == 'Y' and float(x[1])*float(shares) > cash:
+                    print('\nNot enough money to buy %s \n' %(give_cur))
+                    print('\nTotal Cost: ')
+                    print(float(x[1])*float(shares))
+                    print('\nRemaining Cash: ')
+                    print(round(cash,2))
+                if buy_confirm == 'Y' and float(x[1])*float(shares) <= cash:
+                    tradenum += 1
+                    #Add the buy to the blotter
+                    action(trade)
+                    cash = cash - blotter[blotter['Action'] == 'Buy']['Money In/Out'].sum()
+                    update_pl(df_pl, shares)
+                    print('\nBlotter\n')
+                    print(blotter)
+                    print('\nRemaining Cash:\n')
+                    print(round(cash,2))
+                if buy_confirm == 'N':
+                    print('\nDid not buy %s' %(give_cur))
+            if trade == 'Sell':
+                give_cur = input('\nPick your currency to sell: ')
+                rec_cur = input('\nPick your currency to buy: ')
+                shares = float(input('\nEnter Quantity: '))
+                get_graph(give_cur,rec_cur)
+                x = get_quote(give_cur,rec_cur)
+                sell_confirm = input('\nSell %s of %s for %s at %s for %s? (Y/N): ' % (shares, give_cur, rec_cur, x[0], round(float(x[0])*float(shares),2)))
+                if sell_confirm == 'Y':
+                    tradenum += 1
+                    action(trade)
+                    cash = cash + blotter[blotter['Action'] == 'Sell']['Money In/Out'].sum()
+                    update_pl(df_pl, shares)
+                    print('\nBlotter\n')
+                    print(blotter)
+                    print('\nRemaining Cash:\n')
+                    print(round(cash,2))
+                
+        elif selected == 3:
+            #Show blotter
+            print('\nBlotter\n')
+            print(blotter) 
+        
+        elif selected == 4:  
+            view_pl(df_pl)
+
+        elif selected == 5:
+            print('\nThanks')
+            done = False
+            
+        elif selected >5 or selected<1:
+            print('\nPlease enter a valid choice')
+    
 
 #client = MongoClient()
 #db = client.blotter_database
 #collection = db.blotter_collection
 #trades = db.trades
-
-menu = ['Crypto Info', 'Trade','Show Blotter','Show P/L','Quit']
-
-blotter = pd.DataFrame(columns=[
-        'Action',
-        'Bought Currency',
-        'Sold Currency',
-        'Quantity',
-        'Price per Share',
-        'Trade Timestap',
-        'Money In/Out',
-        'Cash']
-        )
 
 def display_menu(menu):
     print('\nMain Menu\n')
@@ -75,12 +197,6 @@ def get_quote(give_cur,rec_cur):
     
     return (ask, bid, minprice, maxprice, stdprice, hist_df['time'] ,hist_df['close'])
 
-def action(trade):
-    x = get_quote(give_cur,rec_cur)
-    if trade =='Buy':
-        blotter.loc[tradenum] = (['Buy', give_cur, rec_cur, float(shares), float(x[1]), pd.to_datetime('now'), round(float(x[1])*float(shares),2),cash-round(float(x[1])*float(shares),2)])
-    if trade == 'Sell':
-        blotter.loc[tradenum] = (['Sell', give_cur, rec_cur, float(shares), float(x[1]), pd.to_datetime('now'), round(float(x[1])*float(shares),2),cash+round(float(x[1])*float(shares),2)])
 
 def get_graph(give_cur,rec_cur):  
     quote = get_quote(give_cur,rec_cur)
@@ -111,87 +227,27 @@ def wavg(group, avg_name, weight_name):
     except ZeroDivisionError:
         return d.mean()
     
-def update_pl(pl, shares):
-    if trade =='Buy': # buy
-        x = get_quote(give_cur,rec_cur)
-        current_qty = pl.at[give_cur,'Position']
-        current_vwap = pl.at[give_cur,'VWAP']
-        new_vwap = blotter.groupby('Bought Currency').apply(wavg, 'Price per Share', 'Quantity')
-        pl.at[give_cur,'Position'] = current_qty + shares
-        pl.at[give_cur,'VWAP'] = new_vwap
-        pl.at[give_cur,'UPL'] = float(x[1])*float(shares)
-    elif trade =='Sell': #sell
-        # TODO recalc UPL, RPL, position
-        print("Insert code handling a sale - recalc UPL,RPL & position")    
 
-def view_pl(df_pl):
-     #TODO Update UPL here
-    print("---- PL")
-    print(df_pl)
-    print()
+
+def view_pl(pl):
+    #update_pl(pl, shares)
+    print("P/L")
+    print(pl)
+    #print()
 
 def initialize_pl(give_cur,rec_cur):
-    col_names = ['Bought Currency','Position','VWAP','UPL','RPL']
+    col_names = ['Bought Currency','Current Market Price','Position','VWAP','UPL','RPL','Total P/L','% of Total Shares']
     pl = pd.DataFrame(columns=col_names)
     for p in blotter['Bought Currency']:
-        data = pd.DataFrame([[p,0,0,0,0]] ,columns=col_names)
+        data = pd.DataFrame([[p,0,0,0,0,0,0,0]] ,columns=col_names)
         pl = pl.append(data, ignore_index=True)
     pl = pl.set_index('Bought Currency')
     return pl
-          
-done = True
 
-while done:
-    display_menu(menu)
-    selected = int(input('\nEnter your choice [1-5]: '))
-    if selected == 1:
-        print('\nCrypto Info')
-        give_cur = input('\nPick crypto to look up: ') 
-        rec_cur = input('\nPick currency to receive: ') 
-        get_graph(give_cur,rec_cur)
-        mavg(give_cur,rec_cur)
-        q = get_quote(give_cur,rec_cur)
-        print('\nCurrent Price: %s' %(q[0]))
-        print('\nMax Price in the Last 24hrs: %s' %(q[1]))
-        print('\nMin Price in the Last 24hrs: %s' %(q[2]))
-        print('\nStd Dev of Price in the Last 24hrs: %s' %(q[3]))  
-    
-    if selected == 2:
-        print('\nTrade Menu')
-        trade = input('Buy or Sell?: ')
-        if trade == 'Buy':
-            give_cur = input('\nPick your currency to buy: ')
-            rec_cur = input('\nPick your currency to sell: ')
-            shares = float(input('\nEnter Quantity: '))
-            get_graph(give_cur,rec_cur)
-            x = get_quote(give_cur,rec_cur)
-            buy_confirm = input('\nBuy %s of %s for %s at %s for %s? (Y/N): ' % (shares, give_cur, rec_cur, x[0], round(float(x[0])*float(shares),2)))
-            #Check to see if the user has enough cash to buy selected number of shares at ask price
-            if buy_confirm == 'Y' and float(x[0])*float(shares) > cash:
-                print('\nNot enough money to buy %s \n' %(give_cur))
-                print('\nTotal Cost: ')
-                print(float(x[0])*float(shares))
-                print('\nRemaining Cash: ')
-                print(round(cash,2))
-            if buy_confirm == 'Y' and float(x[0])*float(shares) <= cash:
-                tradenum += 1
-                #Add the buy to the blotter
-                act = action(trade)
-                pl = initialize_pl(give_cur,rec_cur)
-                update_pl(pl, shares)
-                
-    if selected == 3:
-        view_pl(df_pl)
-        
-                
-                
+if __name__ == "__main__":
+    main()
 
-    
-    
-    
-    elif selected == 5:
-        print('\nThanks')
-        done = False
-        
-    elif selected >5 or selected<1:
-        print('\nPlease enter a valid choice')
+
+
+
+
