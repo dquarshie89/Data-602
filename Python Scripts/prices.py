bbc0bbda49e986ac69325b4a3ac2d6ce2c16f3a2
dquarshie89 <dquarshie89@gmail.com>
2018-04-30 08:13:35 -0400

    Create prices.py

---
+++ Python Scripts/prices.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 29 20:43:08 2018
+
+@author: admin
+"""
+
+
+import pandas as pd
+import time
+import seaborn as sns
+import matplotlib.pyplot as plt
+import datetime
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Activation, Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+split_date='2018-02-05'
+window_len = 10
+
+# get market info for bitcoin from the start of 2017 to the current day
+bitcoin_market_info = pd.read_html("https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20170101&end="+time.strftime("%Y%m%d"))[0]
+# convert the date string to the correct date format
+bitcoin_market_info = bitcoin_market_info.assign(Date=pd.to_datetime(bitcoin_market_info['Date']))
+# when Volume is equal to '-' convert it to 0
+#bitcoin_market_info.loc[bitcoin_market_info['Volume']=="-",'Volume']=0
+# convert to int
+bitcoin_market_info['Volume'] = bitcoin_market_info['Volume'].astype('int64')
+bitcoin_market_info.columns = ['Date','bt_Open','bt_High','bt_Low','bt_Close','bt_Volume','bt_MarketCap']
+
+# get market info for ethereum from the start of 2017 to the current day
+eth_market_info = pd.read_html("https://coinmarketcap.com/currencies/ethereum/historical-data/?start=20170101&end="+time.strftime("%Y%m%d"))[0]
+# convert the date string to the correct date format
+eth_market_info = eth_market_info.assign(Date=pd.to_datetime(eth_market_info['Date']))
+eth_market_info['Volume'] = eth_market_info['Volume'].astype('int64')
+eth_market_info.columns = ['Date','eth_Open','eth_High','eth_Low','eth_Close','eth_Volume','eth_MarketCap']
+
+market_info = pd.merge(bitcoin_market_info,eth_market_info, on=['Date'])
+market_info = market_info[market_info['Date']>='2016-01-01']
+for coins in ['bt_', 'eth_']: 
+    kwargs = { coins+'day_diff': lambda x: (x[coins+'Close']-x[coins+'Open'])/x[coins+'Open']}
+    market_info = market_info.assign(**kwargs)
+
+model_data = market_info[['Date']+[coin+metric for coin in ['bt_', 'eth_'] 
+                                   for metric in ['Close','Volume']]]
+# need to reverse the data frame so that subsequent rows represent later timepoints
+model_data = model_data.sort_values(by='Date')
+
+training_set, test_set = model_data[model_data['Date']<split_date], model_data[model_data['Date']>=split_date]
+training_set = training_set.drop('Date', 1)
+test_set = test_set.drop('Date', 1)
+
+
+norm_cols = [coin+metric for coin in ['bt_', 'eth_'] for metric in ['Close','Volume']]
+
+LSTM_training_inputs = []
+for i in range(len(training_set)-window_len):
+    temp_set = training_set[i:(i+window_len)].copy()
+    for col in norm_cols:
+        temp_set.loc[:, col] = temp_set[col]/temp_set[col].iloc[0] - 1
+    LSTM_training_inputs.append(temp_set)
+LSTM_training_outputs = (training_set['eth_Close'][window_len:].values/training_set['eth_Close'][:-window_len].values)-1
+
+
+LSTM_test_inputs = []
+for i in range(len(test_set)-window_len):
+    temp_set = test_set[i:(i+window_len)].copy()
+    for col in norm_cols:
+        temp_set.loc[:, col] = temp_set[col]/temp_set[col].iloc[0] - 1
+    LSTM_test_inputs.append(temp_set)
+LSTM_test_outputs = (test_set['eth_Close'][window_len:].values/test_set['eth_Close'][:-window_len].values)-1
+
+def build_model(inputs, output_size, neurons, activ_func="linear",
+                dropout=0.25, loss="mae", optimizer="adam"):
+    model = Sequential()
+
+    model.add(LSTM(neurons, input_shape=(inputs.shape[1], inputs.shape[2])))
+    model.add(Dropout(dropout))
+    model.add(Dense(units=output_size))
+    model.add(Activation(activ_func))
+
+    model.compile(loss=loss, optimizer=optimizer)
+    return model
+
